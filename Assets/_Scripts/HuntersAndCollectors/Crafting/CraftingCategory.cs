3ae7a707346cc7d8f88c95b2cb3f8dd422e20379
Stevr0 <150867837+Stevr0@users.noreply.github.com>
2026-02-23 21:05:46 +0800

    Create CraftingCategory.cs

---
+++ Assets/_Scripts/HuntersAndCollectors/Crafting/CraftingCategory.cs
@@ -0,0 +1,14 @@
+namespace HuntersAndCollectors.Crafting
+{
+    /// <summary>
+    /// CraftingCategory
+    /// ------------------------------------------------------------
+    /// Simple categories for the crafting window tabs.
+    /// </summary>
+    public enum CraftingCategory
+    {
+        Tools = 0,
+        Equipment = 1,
+        Building = 2
+    }
+}
